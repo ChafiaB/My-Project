bf376407db41ca226ded9cc284c3cb5aac088578
ChafiaB <chafiaberri@gmail.com>
2023-10-17 06:58:14 -0400

    testclass

--- src/git/ADDGit.java
+++ src/git/ADDGit.java
@@ -5,5 +5,18 @@ public class ADDGit {
 	public static void main(String[] args) {
 		System.out.println("aylan");
 	}
+	
+	public static void gitTest() {
+		
+		System.out.println("cocomelon");
+		
+	}
+	
+	public static void testGit() {
+		
+		System.out.println("dragon");
+		
+	}
+	
 
 }
